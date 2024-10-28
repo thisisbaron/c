e28bafe08aa287cd8dce94a3385ba89788ddae4f
thisisbaron <thisisbaronsun@gmail.com>
2024-10-28 17:22:07 +0800

    Add files via upload

---
+++ 6.2.10逃逸字符.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{   
+    printf("123\b\n456\n");
+    // \b回退一格
+    return 0;
+}
---
+++ 6.2.10逃逸字符2.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("123\bA\n456\n");
+    return 0;
+}
