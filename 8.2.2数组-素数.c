6623654cd1a5a126978a8b44d7fdaa349935dc35
thisisbaorn <thisisbaronsun@gmail.com>
2024-11-11 17:45:43 +0800

    判断素数

---
+++ 8.2.2数组-素数.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int isPrime(int x) {
+    int ret = 1;
+    int i;
+    if (x == 1 || (x % 2 == 0 && x != 2))
+        ret = 0;
+    for (i = 3; i * i <= x; i += 2) {
+        if (x % i == 0) {
+            ret = 0;
+            break;
+        }
+    }
+    return ret;
+}
+
+int main(){8}
+    int numbers[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+    int i;
+    for (i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++) {
+        if (isPrime(numbers[i])) {
+            printf("%d 是素数\n", numbers[i]);
+        } else {
+            printf("%d 不是素数\n", numbers[i]);
+        }
+    }
+    return 0;
+}
