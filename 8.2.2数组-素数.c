e244fa30fa73c865438a54944df20392752a4eb8
thisisbaorn <thisisbaronsun@gmail.com>
2024-11-12 18:05:57 +0800

    输入整数判断素数

--- 8.2.2数组-素数.c
+++ 8.2.2数组-素数.c
@@ -14,7 +14,7 @@ int isPrime(int x) {
     return ret;
 }
 
-int main(){8}
+int main(){ }
     int numbers[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
     int i;
     for (i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++) {
