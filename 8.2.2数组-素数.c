a829c932b2c6615251fbce12498b3be29b5eea27
thisisbaorn <thisisbaronsun@gmail.com>
2024-11-14 16:41:22 +0800

    输入整数判断素数

--- 8.2.2数组-素数.c
+++ 8.2.2数组-素数.c
@@ -14,15 +14,16 @@ int isPrime(int x) {
     return ret;
 }
 
-int main(){ }
-    int numbers[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-    int i;
-    for (i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++) {
-        if (isPrime(numbers[i])) {
-            printf("%d 是素数\n", numbers[i]);
+int main()
+{int numbers;
+printf("输入一个整数：");
+scanf("%d",&numbers); 
+   
+        if (isPrime(numbers)) {
+            printf("%d 是素数\n", numbers);
         } else {
-            printf("%d 不是素数\n", numbers[i]);
+            printf("%d 不是素数\n", numbers);
         }
-    }
+    
     return 0;
 }
