47f7086bf238c53a70cec78a3093b35b5dc9b892
thisisbaorn <thisisbaronsun@gmail.com>
2024-11-17 17:12:04 +0800

    谁做了作业

---
+++ 蓝桥杯/谁做了作业.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+//题目：老师给A、B、C、D、E五个学生布置了一次作业。检查后发现：
+//(1) A和B要么都做了作业，要么都没做。
+//(2) B和C只有一个人做了作业。
+//(3) D没有做作业。
+//(4) C和E最多有一人做了作业。
+//请问谁做了作业。
+int main(int argc, char *argv[])
+{
+  // 请在此输入您的代码
+  printf("C\n");
+  printf("A B\n");
+  printf("A B E");
+  return 0;
+}
