a56141f886ebf6d6ca5c86eb8ac531fa4680e2bc
thisisbaron <thisisbaronsun@gmail.com>
2024-12-02 17:25:53 +0800

    Create 不可兼析取.c
    联结词

---
+++ 蓝桥杯/不可兼析取.c
@@ -0,0 +1,15 @@
+//给定原子变元P、Q的真值(用1表示T，用0表示F)，求命题公式P Q的真值。
+//输入原子变元P、Q的真值(1或0)，用空格隔开。
+//输出命题公式P Q的真值(1或0)。
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  // 请在此输入您的代码
+  int p,q;
+  scanf("%d %d",&p,&q);
+  if(p==q) printf("0");//判断pq是否相等
+  else printf("1");
+  return 0;
+}
