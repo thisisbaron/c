be328c1aa4e4815157e2a75028308959e3add3b3
thisisbaron <thisisbaronsun@gmail.com>
2024-12-05 18:31:40 +0800

    Create 判断推理是否有效.c
    枚举

---
+++ 蓝桥杯/判断推理是否有效.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  // 请在此输入您的代码
+  if(1){
+    printf("yes");
+  }else{
+    printf("no");
+  }
+  return 0;
+}
+//Z→Q 表示如果张老师来了，问题可以得到解答。L→Q表示如果李老师来了，问题可以得到解答。结论 (Z∨L)→Q(Z∨L)→Q 表示如果张老师或李老师来了，问题可以得到解答。
